1f6b1ccdbf37d71e72eb7140f14d84747feb05e9
jow <jmendesnasc2611@gmail.com>
2022-05-07 17:10:29 -0300

    aplicação mvc em aspnet (nao concluido ainda)

---
+++ CursoDIOMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoDIOMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=cursomvc;Integrated Security=True");
+        }
+    }
+}
---
+++ CursoDIOMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CursoDIOMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
